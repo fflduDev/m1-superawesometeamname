e0df455778f20d3f004a3c774b3c9a03ea196462
MichaelConn27 <michael.connors@student.fairfield.edu>
2025-02-12 15:00:13 -0500

    StackImpl methods

--- stack/StackImpl.java
+++ stack/StackImpl.java
@@ -2,53 +2,89 @@ package stack;
 
 public class StackImpl implements Stack {
 
+	private String[] stack; //array for stack values
+	int last = -1; //tracks index of the most recent value
+	
 	@Override
 	public void push(String s) {
-	
+		if (stack == null) {
+			System.out.println("Please set the size of the stack (setCapacity) before pushing");
+			return;
+		}
+		
+		if (last + 1 == stack.length) {
+			System.out.println("Stack is full, cannot add item [" + s + "]");
+			return;
+		}
+		
+		stack[last + 1] = s;
+		last ++;
 		System.out.println("thing i added to stack: " + s);
 	
 	}
 
 	@Override
 	public String pop() {
-		// TODO Auto-generated method stub
-		return null;
+		if (stack == null) {
+			System.out.println("Please set the size of the stack (setCapacity) before poping");
+			return null;
+		}
+		
+		String value = stack[last]; //fetch last value in stack
+		last--; //set index of new last to previous index
+		return value;
+		
 	}
 
 	@Override
 	public Boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return null;
+		if (stack == null || last == -1) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	@Override
 	public Boolean isFull() {
-		// TODO Auto-generated method stub
-		return null;
+		if (stack != null && last + 1 == stack.length) {
+			return true;
+		}
+		return false;
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return last + 1;
 	}
 
 	@Override
 	public String peek() {
-		// TODO Auto-generated method stub
-		return null;
+		if (stack == null) {
+			System.out.println("Please set the size of the stack (setCapacity) before peeking");
+			return null;
+		}
+		return stack[last];
 	}
 
 	@Override
 	public void setCapacity(int size) {
-		// TODO Auto-generated method stub
+		
+		stack = new String[size];
 		
 	}
 
 	@Override
 	public void display() {
-		// TODO Auto-generated method stub
+		if (stack == null) {
+			System.out.println("Please set the size of the stack (setCapacity) before displaying");
+			return;
+		}
 		
+		System.out.println("Displaying current stack values: ");
+		for (int i = 0; i <= last; i++) {
+			System.out.println(stack[i]);
+		}
 	}
  
 
--- stack/StackTester.java
+++ stack/StackTester.java
@@ -6,11 +6,47 @@ public class StackTester {
 
 		Stack stackTester = new StackImpl();
 		stackTester.push("thing in stack");
+		runTests(stackTester);
 
 	}
 	
 	public static void runTests(Stack stack) {
+		//initial empty/full tests on null stack
+		System.out.println("Testing if empty: " + stack.isEmpty());
+		System.out.println("Testing if full: " + stack.isFull());
+		
+		//setting capacity and empty/full tests on non-null stack
+	 	stack.setCapacity(4);
+	 	System.out.println("Testing if empty: " + stack.isEmpty());
+	 	System.out.println("Testing if full: " + stack.isFull());
 	 	
+	 	//adding values
+	 	stack.push("one");
+	 	stack.push("two");
+	 	stack.push("cheese");
+	 	stack.push("a");
+	 	
+	 	//testing display and full
+	 	System.out.println("Testing if full: " + stack.isFull());
+	 	stack.display();
+	 	
+	 	//testing handling of overflow
+	 	stack.push("b");
+	 	
+	 	//pop testing
+	 	String value = stack.pop();
+	 	System.out.println(value);
+	 	System.out.println(stack.pop());
+	 	stack.push("cool");
+	 	System.out.println(stack.pop());
+	 	stack.push("cool");
+	 	
+	 	//final empty/full tests
+	 	System.out.println("Testing if empty: " + stack.isEmpty());
+	 	System.out.println("Testing if full: " + stack.isFull());
+	 	
+	 	//final display test to make sure it stops at proper value (should be "cool")
+	 	stack.display();
 	}
 
 }
